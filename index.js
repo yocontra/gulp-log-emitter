eeabf2ba47d40d5341e4986bf9975c071721870d
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-03-26 10:23:21 -0400

    Fix weird tab change back to 2 spaces

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 var LogEmitter = function (source) {
-	this.source = source;
+  this.source = source;
 };
 
 LogEmitter.prototype.info = function (message) {
-	process.emit("gulp:log", { level: "info", message: message, source: this.source });
+  process.emit("gulp:log", { level: "info", message: message, source: this.source });
 };
 
 LogEmitter.prototype.warn = function (message) {
-	process.emit("gulp:log", { level: "warn", message: message, source: this.source });
+  process.emit("gulp:log", { level: "warn", message: message, source: this.source });
 };
 
 LogEmitter.prototype.error = function (message) {
-	var data = {};
-	data.level = "error";
-	data.message = message;
-	data.source = this.source;
+  var data = {};
+  data.level = "error";
+  data.message = message;
+  data.source = this.source;
 
-	if (message instanceof Error) {
-		data.message = message.message;
-		data.error = message;
-	}
+  if (message instanceof Error) {
+    data.message = message.message;
+    data.error = message;
+  }
 
-	process.emit("gulp:log", data);
+  process.emit("gulp:log", data);
 };
 
 LogEmitter.prototype.debug = function (message) {
-	process.emit("gulp:log", { level: "debug", message: message, source: this.source });
+  process.emit("gulp:log", { level: "debug", message: message, source: this.source });
 };
 
 
 module.exports = {
-	Logger: function (source) {
-		return new LogEmitter(source);
-	}
+  Logger: function (source) {
+    return new LogEmitter(source);
+  }
 };
 
 // var log = require('gulp-logemitter').Logger('my plugin');
